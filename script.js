e1459835e086d23d3a5bb8d7151ea2d107b235fc
AndryxaG <gordeevyzel@gmail.com>
2025-01-24 23:41:15 +0300

    Update script.js

--- script.js
+++ script.js
@@ -43,26 +43,3 @@ buyButtons.forEach(button => {
         alert('Товар добавлен в корзину!');
     });
 });
-document.addEventListener("DOMContentLoaded", () => {
-    let rotation = 20;
-
-    const btn = document.querySelector(".engine");
-
-    btn.addEventListener("click", () => {
-        btn.classList.toggle("active");
-    });
-
-    /* optional
-    window.addEventListener("keydown", (event) => {
-        const wrapper = document.getElementById("intro-wrapper");
-
-        if (event.key === "ArrowUp") {
-            rotation += 5;
-            wrapper.style.transform = `rotateX(${rotation}deg) translateY(-110px)`;
-        } else if (event.key === "ArrowDown") {
-            rotation -= 5;
-            wrapper.style.transform = `rotateX(${rotation}deg) translateY(-110px)`;
-        }
-    });*/
-  
-});
